3525c6a8ff956d66c91b0fb643d7d1c8cc10b0cd
Nikita bhoi <114177037+nikitasanjubhoi@users.noreply.github.com>
2025-09-14 20:35:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-const makeChange = (c) => {
-  // your name here
-};
+// const makeChange = (c) => {
+//   c = parseInt(c); // Convert input to number (from prompt)
 
-// Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+//   let q = Math.floor(c / 25);
+//   c = c % 25;
+
+//   let d = Math.floor(c / 10);
+//   c = c % 10;
+
+//   let n = Math.floor(c / 5);
+//   c = c % 5;
+
+//   let p = c;
+
+//   return { q, d, n, p };
+// };
+
+// // Do not change the code below
+// const c = prompt("Enter c: ");
+// alert(JSON.stringify(makeChange(c)));
